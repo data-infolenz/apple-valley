bd2daf65200f33cf239b00e044d775eef0d3aa0d
data-infolenz <data-infolenz@users.noreply.github.com>
2026-08-14 19:19:25 +0530

    Remove Prisma generate from Next config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,3 @@
-const { execSync } = require('child_process');
-
-if (process.env.SKIP_PRISMA_GENERATE !== '1') {
-  execSync('npx prisma generate', { stdio: 'inherit' });
-}
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   async headers() {
